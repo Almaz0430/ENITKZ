a5d4db60b2849242c756499deb064875bef714e3
Almaz0430 <almaz.zh7@gmail.com>
2025-05-28 00:10:30 +0500

    Добавлен фронтенд в Django (Vite build)

--- backend/urls.py
+++ backend/urls.py
@@ -65,3 +65,4 @@ urlpatterns = [
 # Добавление URL для медиа-файлов в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
